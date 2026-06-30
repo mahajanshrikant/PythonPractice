d06f40d4f11f672b6b6a14d87386ad244bf4500c
Shrikant Mahajan <shrikantmahajan10@gmail.com>
2026-06-30 08:04:46 +0530

    python syntax : learning 30_06_2026

--- main.py
+++ main.py
@@ -272,5 +272,8 @@ from Enemy import *
 
 enemy=Enemy()
 enemy.type_of_enemy="Zombie"
+enemy.talk()
+enemy.walk_forward()
+enemy.attack()
 print(f'{enemy.type_of_enemy} has {enemy.health_points} health points And can do attack of {enemy.attack_damage}  ')
 

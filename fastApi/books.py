77d7d23a13ba30226339958e885185944874e0e5
Shrikant Mahajan <shrikantmahajan10@gmail.com>
2026-06-30 10:07:45 +0530

    python syntax : learning 30_06_2026-fast  api endpoint post

--- fastApi/books.py
+++ fastApi/books.py
@@ -1,8 +1,54 @@
-from fastapi import FastAPI
+from fastapi import Body,FastAPI
 
 app = FastAPI()
 
-@app.get("/api-endpoint")
-async def first_Api():
-    return {"hello": "world"}
+Books=[ {'title':'My first book','author':'Author one','category':'science'},
+        {'title':'My seocnd book','author':'Author second','category':'science'},
+        {'title':'My third book','author':'Author third','category':'science'},
+        {'title':'My fourth book','author':'Author fourth','category':'math'}
+    ]
+
+
+
+
+@app.get("/books")
+async def read_all_books():
+    return Books
+
+
+@app.get("/books/mybook")
+async def  read_all_books():
+    return {'book_title':'My first book','book_category':'science'}
+
+##Path parameter
+@app.get("/books/{book_title}")
+async  def read_All_book(book_title:str):
+    for book in Books:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
+
+## Query Parameter
+@app.get("/books/")
+async def read_catgeory_by_query(category:str):
+    books_to_return=[]
+    for book in Books:
+        if book.get('category').casefold()==category.casefold():
+            books_to_return.append(book)
+            return books_to_return
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query( book_author:str,category:str):
+    books_to_return=[]
+    for book in Books:
+        if  book.get('author').casefold()==book_author.casefold() and book.get('category').casefold()==category.casefold():
+            books_to_return.append(book)
+            return books_to_return
+
+
+
+####//Post Method
+@app.post("/book/create_book")
+async def create_book(new_book=Body()):
+    Books.append(new_book)
+
 
